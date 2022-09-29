f361bf6d29b3842713d5f03e3f326c1875135a47
ujjwal1103 <ujjwallade11@gmail.com>
2022-09-29 10:49:24 +0530

    sep29

---
+++ sep28/arrowFuction.js
@@ -0,0 +1,29 @@
+
+var number = 25
+
+
+const isOdd = number => number % 2 !== 0
+
+console.log(isOdd(number))
+
+// Arrow function
+const isOddArray = [2,6,8].every(element => element % 2 === 0) 
+
+
+console.log(isOddArray)
+
+
+
+// const callBack = .every(isOdd)
+
+// console.log(callBack);
+
+
+// for(const i = 0;i<9;i++){
+//     console.log(i);
+// }
+
+var n = 98888888888888888
+
+console.log(n.toExponential(2));
+
---
+++ sep28/oops/opps.js
@@ -0,0 +1,15 @@
+class JsClass{
+
+    
+    constructor(name){
+        this.name = name;
+    }
+
+    getColor(){
+        return this.name;
+    }
+}
+
+
+
+let js = new JsClass("ujjwal");
